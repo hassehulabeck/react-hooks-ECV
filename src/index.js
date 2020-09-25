8d99c6a1f5175fc2c954fea7370d26a274203778
hassehulabeck <hansbudandersson@gmail.com>
2020-09-25 15:07:44 +0200

    First

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Multiplikator() {
+    const [numbers, setNumbers] = useState([4, 3]);
+    const [produkt, setProdukt] = useState(0);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <div>
+            <p>
+                {numbers[0]} och {numbers[1]}
+            </p>
+            <p>{produkt}</p>
+            <button onClick={() => setProdukt(numbers[0] * numbers[1])}>
+                Multiplicera
+            </button>
+        </div>
+    );
+}
+
+ReactDOM.render(<Multiplikator />, document.getElementById("root"));
