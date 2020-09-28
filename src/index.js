88cf29801dc26f737e89aab9414c69b1d9fa256c
hassehulabeck <hansbudandersson@gmail.com>
2020-09-28 11:45:24 +0200

    nsåg

--- src/index.js
+++ src/index.js
@@ -5,16 +5,27 @@ import "./index.css";
 function Firstname(props) {
     const [firstname, setFirstname] = useState(props.firstname);
 
-    const onSubmit = (e) => {
+    const nameSubmitted = (e) => {
         e.preventDefault();
         setFirstname(e.target.firstname.value);
+    };
+
+    const nameChanger = (e) => {
+        if (e.target.value.length > 3) {
+            firstname = e.target.value;
+            setFirstname(e.target.value);
+        }
     };
 
     return (
         <div>
             <p>{firstname}</p>
-            <form onSubmit={onSubmit}>
-                <input type="text" name="firstname" />
+            <form onSubmit={nameSubmitted}>
+                <input
+                    type="text"
+                    name="firstname"
+                    onChange={(e) => nameChanger(e)}
+                />
                 <input type="submit" value="Ändra namn" />
             </form>
         </div>
