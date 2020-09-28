587f7f48a2aea2fe2c7ee37367f6ba72d92ae1f0
hassehulabeck <hansbudandersson@gmail.com>
2020-09-28 10:38:28 +0200

    Used named function in function

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function Multiplikator() {
-    const [numbers, setNumbers] = useState([4, 3]);
-    const [produkt, setProdukt] = useState(0);
+    const numbers = [4, 3];
+    const [total, setTotal] = useState(0);
+
+    function calculate() {
+        setTotal(numbers[0] * numbers[1]);
+    }
 
     return (
         <div>
             <p>
                 {numbers[0]} och {numbers[1]}
             </p>
-            <p>{produkt}</p>
-            <button onClick={() => setProdukt(numbers[0] * numbers[1])}>
-                Multiplicera
-            </button>
+            <p>{total}</p>
+            <button onClick={() => calculate()}>Multiplicera</button>
         </div>
     );
 }
